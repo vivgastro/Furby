4ae4f855af44696bd09845ce2cdef76a3c267d5c
Vivek Gupta <vgupta@mpsr-srv0.obs.molonglo.local>
2019-02-21 04:23:50 +1100

    removed one by one viewing option

--- check_furby.py
+++ check_furby.py
@@ -115,9 +115,9 @@ def main(args):
         M.setp(ax1.get_xticklabels(), visible=False)
         M.setp(ax3.get_yticklabels(), visible=False)
         #M.setp(ax2.get_yticklabels(), visible=False)
-	if not args.one:     
-	    mgr=M.get_current_fig_manager()
-            mgr.window.move((k%3)*640, int(k/3)*600)
+	#if not args.one:     
+	#    mgr=M.get_current_fig_manager()
+        #    mgr.window.move((k%3)*640, int(k/3)*600)
 
         if args.pngs:
             print "saving",fil
@@ -127,9 +127,8 @@ def main(args):
 
         if(k<len(args.furbies)-1):
             M.show(block=False)
-	    if args.one:
-		raw_input("<Press Enter to see next plot>\n")
-		M.close('all')
+	    raw_input("<Press Enter to see next plot>\n")
+	    M.close('all')
     if not args.pngs:
         M.show()
 
@@ -139,7 +138,6 @@ if __name__=="__main__":
     a.add_argument("-dd", "--dedisp", action='store_true', help="Dedisperse the furby? (def=False)", default=False)
     a.add_argument("-fs","--freq_sc", type=int, help="Freq scrunch factor (def=1)", default=1)
     a.add_argument("-ts","--t_sc", type=int, help="Time scrunch factor (def=1)", default=1)
-    a.add_argument("-one", action='store_true', help="Show plots one by one (def=False)", default=False)
 
     a.add_argument("-pngs", action='store_true', help="Save pngs instead of plotting")
     args=a.parse_args()
